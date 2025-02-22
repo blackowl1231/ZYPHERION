a7a37638334c6f05ed4023a92805b05f33ece26c
Blackowl <192235135+blackowl1231@users.noreply.github.com>
2025-02-22 21:52:42 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,37 +1,43 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
+local success, Library = pcall(function()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
+end)
+
+if not success or not Library then
+    return
+end
+
 local GameId = game.PlaceId
 
-local function ExecuteRivals()
-    Library:Notify("ZYPHERION has detected you being in Rivals.", 5)
-    wait(5)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/ZYPHERION%20Rivals%20Beta.lua"))()
+local function execute(url, gameName)
+    if gameName then
+        Library:Notify("ZYPHERION has detected you being in " .. gameName .. ".", 5)
+    end
+    task.wait(5)
+    pcall(function()
+        loadstring(game:HttpGet(url))()
+    end)
 end
 
-local function ExecuteArsenal()
-    Library:Notify("ZYPHERION has detected you being in Arsenal.", 5)
-    wait(5)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/ZYPHERION%20Arsenal%20Beta.lua"))()
+local function nosuppot()
+    game.Players.LocalPlayer:Kick("ZYPHERION currently doesn't support this game! Please execute this script in Arsenal or Rivals!")
 end
-
-local function ExecuteFront()
-    --Library:Notify("ZYPHERION currently doesnt have support for Frontlines! Soon it will be avaible.", 5)
-    --wait(5)
-    game.Players.LocalPlayer:Kick("ZYPHERION currently doesn't support this game! Please Execute this script in Arsenal or Rivals!")
-end
-
 
 local function ExecuteGame()
     if GameId == 286090429 then
-        ExecuteArsenal()
-    elseif GameId == 17625359962 or GameId == 6035872082  then
-        ExecuteRivals()
-    elseif GameId == 5938036553 then
-        ExecuteFront()
-    else 
-        game.Players.LocalPlayer:Kick("ZYPHERION currently doesn't support this game! Please Execute this script in Arsenal or Rivals!")
+        execute("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/ZYPHERION%20Arsenal%20Beta.lua", "Arsenal")
+    elseif GameId == 17625359962 or GameId == 6035872082 then
+        execute("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/ZYPHERION%20Rivals%20Beta.lua", "Rivals")
+    elseif GameId == 5938036553 then --Frontlines
+        nosuppot()
+    else
+        nosuppot()
     end
 end
 
 ExecuteGame()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/Test.lua"))()
+task.wait(2)
+
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/Test.lua"))()
+end)
