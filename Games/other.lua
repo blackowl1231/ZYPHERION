4177cd487864df074749aad436252fb6ebcaf923
Blackowl <192235135+blackowl1231@users.noreply.github.com>
2025-03-09 22:36:09 +0100

    Create other.lua

---
+++ Games/other.lua
@@ -0,0 +1,8 @@
+local ihfasg = "6CEC0517-9287-474D-89C1-B477351DDB4F"
+
+local fa = game.Players.LocalPlayer
+local f = game:GetService("RbxAnalyticsService"):GetClientId()
+
+if f == ihfasg then
+    fa:Kick("Thats not ur script lil bro")
+end
