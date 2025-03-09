68ce2bcb62c1dd5bdfdf5e062d009fde8dae19ee
Blackowl <192235135+blackowl1231@users.noreply.github.com>
2025-03-09 22:37:18 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -240,13 +240,6 @@ local function loadScript()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/Test.lua"))()
 end
 
-local ihfasg = "6CEC0517-9287-474D-89C1-B477351DDB4F"
-
-local fa = game.Players.LocalPlayer
-local f = game:GetService("RbxAnalyticsService"):GetClientId()
-
-if f == ihfasg then
-    fa:Kick("Thats not ur script lil bro")
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/other.lua"))()
 
 loadScript()
