fe8352cc3491077617ba2b19245df0fbe10e333f
Blackowl <192235135+blackowl1231@users.noreply.github.com>
2025-03-10 06:22:22 +0100

    Update other.lua

--- Games/other.lua
+++ Games/other.lua
@@ -1,6 +1,6 @@
 local ihfasg = "6CEC0517-9287-474D-89C1-B477351DDB4F"
 
-local fa = game.Players.LocalPlayer
+local fa = game:GetService("Players").LocalPlayer
 local f = game:GetService("RbxAnalyticsService"):GetClientId()
 
 if f == ihfasg then
