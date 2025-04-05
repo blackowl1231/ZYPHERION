550750bc96f9b25138174c7d1f910f7e6613626d
Blackowl <192235135+blackowl1231@users.noreply.github.com>
2025-04-05 14:11:39 +0200

    Update other.lua

--- Games/other.lua
+++ Games/other.lua
@@ -1,8 +1,8 @@
-local hwid = "6CEC0517-9287-474D-89C1-B477351DDB4F"
+local hwid = {"AF11C64B-5D4D-4333-8512-ABFDC76EAB2B","6CEC0517-9287-474D-89C1-B477351DDB4F"}
 
 local lil = game:GetService("Players").LocalPlayer
 local bro = game:GetService("RbxAnalyticsService"):GetClientId()
 
 if bro == hwid then
-    lil:Kick("Thats not ur script lil bro")
+    lil:Kick("Blacklisted")
 end
